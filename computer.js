167ec09499f0105f3bf72bb680ad63eea9647d35
himangshu06 <76275772+himangshu06@users.noreply.github.com>
2021-08-11 20:20:50 +0530

    Add files via upload

---
+++ computer.js
@@ -0,0 +1,39 @@
+img ="";
+status = "";
+object = [];
+function preload(){
+    img = loadImage("PC_R.jpg");
+}
+function setup(){
+    canvas = createCanvas(650,400);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status: detecting Objects";
+}
+function modelLoaded(){
+    console.log("modelLoaded");
+    status = true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if (error){
+    console.log(error);
+    }
+    console.log(results);
+    object = results;
+}
+function draw(){
+    image(img,0,0,650,400);
+    if (status != ""){
+        for(i = 0;i <object.length;i++){
+            document.getElementById("status").innerHTML="status: Object Detecting";
+            fill("#00FF00");
+            percent = floor(object[i].confidence*100);
+            text(object[i].label+""+percent+"%",object[i].x,object[i].y);
+            noFill();
+            stroke("#00FF00");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+        }
+    }
+
+}
---
+++ main.js
@@ -0,0 +1,30 @@
+function Dog(){
+    window.location="dog.html";
+    window.location.replace="dog.html";
+}
+function car(){
+    window.location="car.html";
+    window.location.replace="car.html";
+}
+function Tv(){
+    window.location="Tv.html";
+    window.location.replace="Tv.html";
+}
+function Pc(){
+    window.location="computer.html";
+    window.location.replace="computer.html";
+}
+song = "";
+
+function preload(){
+    song=loadSound("ES_Taking Polaris - DEX 1200.mp3");
+}
+function setup(){
+
+}
+
+
+
+
+
+
